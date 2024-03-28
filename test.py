c732ba22040be4640c3585824804cecbcc462d58
BrookAT <142592569+BrookAT@users.noreply.github.com>
2024-03-28 19:48:09 +0330

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import random
+
+a = random.randint(0,55)
+
+
+while True:
+  b = input("Give me a number: ":int)
+  while b != a
+  if b > a :
+    print("Dash Adadet Bozorge")
+  elif b < a :
+    print("dash adader Kochike")
+  else : print ("Chi migi")
+    break
